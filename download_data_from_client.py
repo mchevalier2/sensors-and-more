Fix minor bug. The assignment to NULL values did not account units.

download_data_from_client.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
                     raise Exception(f"Non-success status code: {response.status_code}")
 
                 if np.random.rand() < 0.01:
-                    dict1.update({[x for x in dict1][np.random.randint(0, 4)]: "NULL"})
+                    dict1.update({[x for x in dict1][np.random.randint(0, len(dict1))]: "NULL"})
 
                 sensor_data.append(dict1)
     df = pd.DataFrame(sensor_data).reindex(